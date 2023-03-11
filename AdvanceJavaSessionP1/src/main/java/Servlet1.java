08ce03d8ebdbdad2d88f20bca7aa4499fb665f5f
xyz <xyz@example.com>
2023-03-11 06:56:32 +0530

    11-03-2023

---
+++ AdvanceJavaSessionP1/src/main/java/Servlet1.java
@@ -0,0 +1,47 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Servlet1
+ */
+@WebServlet("/Servlet1")
+public class Servlet1 extends HttpServlet 
+{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		PrintWriter out=resp.getWriter();
+		
+		String uname=req.getParameter("uname");
+		String pass=req.getParameter("pass");
+		
+		if(uname.equals("admin")&&pass.equals("admin"))
+		{
+			HttpSession sess=req.getSession();
+			sess.setAttribute("u1",uname);
+			RequestDispatcher rd=req.getRequestDispatcher("Servlet2");
+			rd.forward(req, resp);
+			
+		}
+		else
+		{
+			Cookie ck=new Cookie("u1",uname);
+			resp.addCookie(ck);
+			//resp.sendRedirect("Servlet3");
+			RequestDispatcher rd=req.getRequestDispatcher("Servlet3");
+			rd.include(req, resp);
+		}
+	}
+
+}
