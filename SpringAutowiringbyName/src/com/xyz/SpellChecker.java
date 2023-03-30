0d9b2ac9d80c70983ecab1735677ec162e26ae2e
OnlineTraning <57015977+Onlinetrainingtech@users.noreply.github.com>
2023-03-30 21:09:41 +0530

    30-03-2023

---
+++ SpringAutowiringbyName/src/com/xyz/SpellChecker.java
@@ -0,0 +1,13 @@
+package com.xyz;
+
+public class SpellChecker 
+{
+   public SpellChecker()
+   {
+	   System.out.println("Inside the SpellChecker");
+   }
+   public void checkSpelling()
+   {
+	   System.out.println("Inside the CheckSpelling");
+   }
+}
